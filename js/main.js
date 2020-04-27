1fcba2aad725eb4ad324379392cdd2235ab2636e
ParkerOHeeron <parkeroheeron@gmail.com>
2020-04-27 05:35:30 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
-console.log("yo");
-let cardOne = "queen";
-let cardThree = "queen";
-let cardTwo, cardFour  = "king";
-console.log("User flipped " + cardOne);
+const cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+/*console.log("User flipped" + poppedcard);*/
+cardsInPlay.push(cardTwo);
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] == cardsInPlay[1]){
+		alert("You found a match!");
+	}else{
+		alert("Sorry, try again.");
+	}
+}
